0077a70f99134364f357a85ef27844eb98e033c1
Thiago Brito <thiagosbrito@Thiagos-MacBook-Pro-2.local>
2024-07-14 12:34:38 +0200

    Removing openai imports

--- utils/getColorName.ts
+++ utils/getColorName.ts
@@ -1,19 +1,19 @@
-// utils/getColorName.js
-import { Configuration, OpenAIApi } from 'openai';
-
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-});
-
-const openai = new OpenAIApi(configuration);
-
-export const getColorName = async (colorHex: string) => {
-    const prompt = `What is a good name for the color with hex code ${colorHex}?`;
-    const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt,
-        max_tokens: 10,
-    });
-
-    return response.data.choices[0].text.trim();
-};
+// // utils/getColorName.js
+// import Configuration, { OpenAI } from 'openai';
+//
+// const configuration = new Configuration({
+//     apiKey: process.env.OPENAI_API_KEY,
+// });
+//
+// const openai = new OpenAI(configuration);
+//
+// export const getColorName = async (colorHex: string) => {
+//     const prompt = `What is a good name for the color with hex code ${colorHex}?`;
+//     const response = await openai.createCompletion({
+//         model: "text-davinci-003",
+//         prompt,
+//         max_tokens: 10,
+//     });
+//
+//     return response.data.choices[0].text.trim();
+// };
