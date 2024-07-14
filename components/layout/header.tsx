72238e8557dee553f78e6ee35c582b43e6bd248f
Thiago Brito <thiagosbrito@Thiagos-MacBook-Pro-2.local>
2024-07-14 12:30:37 +0200

    First commit

---
+++ components/layout/header.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import Link from "next/link";
+import {Button} from "@/components/ui/button";
+import {GithubIcon} from "lucide-react";
+import {ModeToggle} from "@/components/layout/theme-toggle";
+
+export default function Header() {
+    return (
+        <div className="w-full h-20 px-6 flex flex-row items-center justify-between">
+            <div className="">
+                <Image
+                    src="svg/logo_only.svg"
+                    alt="logo"
+                    width={150}
+                    height={70}
+                ></Image>
+            </div>
+            <nav className="flex-1 flex flex-row items-center justify-center center">
+                <ul className="flex-row items-center gap-x-12 hidden md:flex">
+                    <li>
+                        <Link
+                            className="font-sans font-semibold text-lg"
+                            href={"/"}
+                        >
+                            Color Palette
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            className="font-sans font-semibold text-lg"
+                            href={"/"}
+                        >
+                            Generate Gradient
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+            <div className="flex flex-row gap-x-3">
+                <Button
+                    className="gap-x-2"
+                >
+                    <GithubIcon size={18}/>
+                    <span className="hidden md:inline-block">
+                        Sign In with GitHub
+                    </span>
+                </Button>
+                <ModeToggle/>
+            </div>
+        </div>
+    )
+}
